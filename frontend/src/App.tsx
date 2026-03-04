037f0a371387b8d4a2e5a15d5fb791a51ad4c4dd
Jacob Hale <Jchale23@byui.edu>
2026-03-04 16:24:43 -0700

    followed JSON video

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,21 @@
 import "./App.css";
 
+function Welcome() {
+  return <h1>Criminally Underrated Bands</h1>;
+}
+
 function BandList() {
-  const bandNames = ["Dire Straits", "R.E.M.", "Collective Soul"];
+  const bandNames = [
+    {name: 'Dire Straits', members: 'Mark, David, John, Pick', formed: 1977},
+    {name: 'R.E.M', members: "Micheal, Peter, Mike, Bill", formed: 1980},
+    {name: "Collective Soul", members: "Ed, Dean, David, Ross, Shane", formed: 1992}
+  ];
 
   return (
     <>
-      <h2>{bandNames[0]}</h2>
-      <h2>{bandNames[1]}</h2>
-      <h2>{bandNames[2]}</h2>
+      <h2>{bandNames[0].name}</h2>
+      <h2>{bandNames[1].name}</h2>
+      <h2>{bandNames[2].name}</h2>
     </>
   );
 }
@@ -15,6 +23,7 @@ function BandList() {
 function App() {
   return (
     <>
+      <Welcome />
       <BandList />
     </>
   );
