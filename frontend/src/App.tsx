ea37e06069bf4d5f444c9fc861c525852c42eab9
Jacob Hale <Jchale23@byui.edu>
2026-03-04 16:32:29 -0700

    followed the props video

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,35 @@
 import "./App.css";
 
+const bandNames = [
+    { name: "Dire Straits", members: "Mark, David, John, Pick", formed: 1977 },
+    { name: "R.E.M", members: "Micheal, Peter, Mike, Bill", formed: 1980 },
+    {
+      name: "Collective Soul",
+      members: "Ed, Dean, David, Ross, Shane",
+      formed: 1992,
+    },
+  ];
+
 function Welcome() {
   return <h1>Criminally Underrated Bands</h1>;
 }
 
+function Band({name, members, formed}: {name: string; members: string; formed: number}){
+  return (
+    <>
+      <h2>{name} </h2>
+      <h3>Original Members: {members}</h3>
+      <h3>Formed: {formed}</h3>
+    </>
+  );
+}
+
 function BandList() {
-  const bandNames = [
-    {name: 'Dire Straits', members: 'Mark, David, John, Pick', formed: 1977},
-    {name: 'R.E.M', members: "Micheal, Peter, Mike, Bill", formed: 1980},
-    {name: "Collective Soul", members: "Ed, Dean, David, Ross, Shane", formed: 1992}
-  ];
+  
 
   return (
     <>
-      <h2>{bandNames[0].name}</h2>
-      <h2>{bandNames[1].name}</h2>
-      <h2>{bandNames[2].name}</h2>
+      <Band/>
     </>
   );
 }
